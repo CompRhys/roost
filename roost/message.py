fixes: one-hot, features: alpha scaling on Robust loss

roost/message.py
@@ -122,7 +122,7 @@ class Roost(nn.Module):
         # define a global pooling function for materials
         mat_heads = 3
         mat_hidden = [256]
-        # msg_hidden = [256]
+        msg_hidden = [256]
         self.cry_pool = nn.ModuleList([WeightedAttention(
             gate_nn=SimpleNetwork(elem_fea_len, 1, mat_hidden),
             message_nn=SimpleNetwork(elem_fea_len, elem_fea_len, msg_hidden),

roost/utils.py
@@ -168,23 +168,23 @@ def load_previous_state(path, model, device, optimizer=None,
     return model, optimizer, normalizer, scheduler, best_error, start_epoch
 
 
-def RobustL1(output, log_std, target):
+def RobustL1(output, log_std, target, alpha=1.0):
     """
     Robust L1 loss using a lorentzian prior. Allows for estimation
     of an aleatoric uncertainty.
     """
     loss = np.sqrt(2.0) * torch.abs(output - target) * \
-        torch.exp(- log_std) + log_std
+        torch.exp(- log_std) + alpha*log_std
     return torch.mean(loss)
 
 
-def RobustL2(output, log_std, target):
+def RobustL2(output, log_std, target, alpha=1.0):
     """
     Robust L2 loss using a gaussian prior. Allows for estimation
     of an aleatoric uncertainty.
     """
     loss = 0.5 * torch.pow(output - target, 2.0) * \
-        torch.exp(- 2.0 * log_std) + log_std
+        torch.exp(- 2.0 * log_std) + alpha*log_std
     return torch.mean(loss)
 
 